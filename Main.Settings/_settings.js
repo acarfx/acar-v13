6cf2138a376699b0951f865284d347e1dd9bfbf5
acar <77089894+acarfx@users.noreply.github.com>
2021-08-08 12:48:29 +0300

    ❤

--- Main.Settings/_settings.js
+++ Main.Settings/_settings.js
@@ -2,11 +2,11 @@ module.exports = {
 
     embed: {
         altbaşlık: "Developed BY ACAR",
-        başlık: "TEST SUNUCU :)",
+        başlık: "Sunucu Ismi",
         renk: "RANDOM",
     },
 
-    Owner: ["852681367853465610"],
+    Owner: [],
     prefix: [".","!"],
-    token: "ODY0OTM5MzAxNTY1MjM1MjMw.YO8vfw.3wkDMQCwdAeAGh8IzMQXCDTJG2g"
+    token: ""
 }
